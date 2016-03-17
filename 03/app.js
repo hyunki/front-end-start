d2292d5625bf1217922dcd3b6bb154027ddf5b2f
hyunki <hyunki08@gmail.com>
2016-03-17 17:47:07 +0900

    class

--- 03/app.js
+++ 03/app.js
@@ -4,4 +4,29 @@
 // document.body.appendChild(div);
 
 // document.body.innerHTML = '<div style="1px solid red">111</div>'
-document.body.innerHTML += '<div style="1px solid red">111</div>'
+// document.body.innerHTML += '<div style="1px solid red">111</div>'
+
+// window.onclick=function(){
+//     alert("load");
+// };
+
+// window.addEventListener('load', function(){
+//     alert("load1");
+// });
+//
+// window.addEventListener('load', function(){
+//     alert("load2");
+// });
+
+// var head = document.querySelectorAll('#head');
+// head[0].addEventListener('click', function(event){
+//   debugger;
+//   console.log(1);
+// });
+
+var clickCount = 0;
+function popup(event){
+    console.log(++clickCount);
+    console.log(arguments, this ,event ,event.type,event.currentTarget);
+}
+document.body.addEventListener('click', popup);
