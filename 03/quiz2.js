e09785b2c9c379d6273f2f431bec7aae5deb58d4
hyunki <hyunki08@gmail.com>
2016-03-23 13:55:00 +0900

    class

--- 03/quiz2.js
+++ 03/quiz2.js
@@ -1,12 +1,14 @@
 var board = document.getElementById("board");
+var str = ' ';
 
-for(var i=0; i<4; i++){
-    for(var j=0; j<4; j++){
-      if(j%2) {
-        document.body.innerHTML += '<span class="black"></span>'
-      } else {
-        document.body.innerHTML += '<span class="white"></span>'
-      }
-        // TODO : 여기에 필요한 코드를 작성해주세요.
+for(var i = 0; i < 4; i++){
+	str += '<div class="line">';
+    for(var j = 0; j < 4; j++){
+     	if((j + i) % 2)
+       		str += '<span class="black"></span>';
+    	else
+       		str += '<span class="white"></span>';
     }
+    str += '</div>';
 }
+board.innerHTML = str;
