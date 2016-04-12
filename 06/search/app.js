fc77266b547c7e248a6ae22735441adb955971ae
hyunki <hyunki08@gmail.com>
2016-04-12 13:43:27 +0900

    class

--- 06/search/app.js
+++ 06/search/app.js
@@ -1,16 +1,63 @@
 console.log('app.js');
 
-var template = document.getElementsByTagName('searchListTemplate').innerHTML;
 var apikey = 'c60e089e4d7ff43ecdda949a441fb9da';
-var q = '제주대';
-var apiurl = 'https://apis.daum.net/search/web?q='+q+'&apikey='+apikey+'&output=json';
 
-function render(wrap, template, data) {
-  var html = tmpl(template, {list: data});
-  wrap.innerHTML = html;
+var searchBtn = document.getElementById('searchBtn');
+var template = document.getElementById('searchListTemplate').innerHTML;
+var wrap = document.getElementById('wrap');
+var wrapBottom = document.getElementById('wrapBottom');
+var pageno = 1;
+var listLen = 10;
+
+function searchBtnClick(event){
+	pageno = 1;
+	listLen = 10;
+	wrap.innerHTML = ' ';
+	wrapBottom.innerHTML = ' ';
+
+	var searchStr = document.getElementById('searchStr').value;
+	var apiurl = 'https://apis.daum.net/search/web?q=' + searchStr + '&apikey=' + apikey 
+				+ '&output=json&result=20&pageno=' + pageno;
+
+	getJSON(apiurl , function(res) {
+		console.log(res.channel);
+
+		var html = ' ';
+
+		for(var i = 0; i < listLen; i++) {
+			html = tmpl(template, {list:res.channel.item[i]});
+			wrap.innerHTML += html;
+		}
+		wrapBottom.innerHTML += '<button id=\"moreBtn\" onclick=\"moreBtnClick()\">more</button>';
+	});
 }
 
-getJSON(apiurl , function(res) {
-  console.log(res);
-  render(wrap, template, res.data);
-});
+function moreBtnClick() {
+	var oldListLen = listLen;
+	var searchStr = document.getElementById('searchStr').value;
+	var apiurl = 'https://apis.daum.net/search/web?q=' + searchStr + '&apikey=' + apikey 
+				+ '&output=json&result=20&pageno=' + pageno;
+	
+	listLen += 10;
+
+	getJSON(apiurl, function(res){
+		console.log(res.channel);
+
+		var html = ' ';
+
+		for(var i = oldListLen; i < listLen; i++) {
+			html = tmpl(template, {list:res.channel.item[i]});
+			wrap.innerHTML += html;
+		}
+		
+		if(listLen >= 20 ) {
+			pageno++;
+			listLen = 0;
+			if(pageno > 3) {
+				wrapBottom.innerHTML = ' ';
+			}	
+		}
+	});
+}
+
+searchBtn.addEventListener('click', searchBtnClick);
